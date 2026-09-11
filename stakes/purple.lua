e8ea12c2fc48bbafd45d00d61c754d1b77c0fcfe
MetaNite64 <noahtrivedi@gmail.com>
2026-09-11 17:17:54 -0500

    fix purple stake not doing anything

--- stakes/purple.lua
+++ stakes/purple.lua
@@ -6,3 +6,8 @@ SMODS.Stake:take_ownership("purple", {
     G.GAME.modifiers.srdx_double_showdowns = true
   end,
 })
+
+local showdown_ref = SMODS.is_showdown_ante
+SMODS.is_showdown_ante = function()
+  return G.GAME.modifiers.srdx_double_showdowns and (G.GAME.round_resets.ante % math.floor(G.GAME.win_ante / 2) == 0 and G.GAME.round_resets.ante > 0) or showdown_ref()
+end
